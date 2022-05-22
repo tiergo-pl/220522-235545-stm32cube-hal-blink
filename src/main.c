2cf83840202875208f91229f6128ad53b6c93d43
Tiergo <tiergo@tiergo.pl>
2022-05-23 01:10:42 +0200

    birth

---
+++ src/main.c
@@ -0,0 +1,144 @@
+#if F0
+#include "stm32f0xx_hal.h"
+#elif F1
+#include "stm32f1xx_hal.h"
+#elif F2
+#include "stm32f2xx_hal.h"
+#elif F3
+#include "stm32f3xx_hal.h"
+#elif F4
+#include "stm32f4xx_hal.h"
+#elif F7
+#include "stm32f7xx_hal.h"
+#elif L0
+#include "stm32l0xx_hal.h"
+#elif L1
+#include "stm32l1xx_hal.h"
+#elif L4
+#include "stm32l4xx_hal.h"
+#else
+#error "Unsupported STM32 Family"
+#endif
+
+#ifdef BLUEPILL
+#define LED_PIN GPIO_PIN_13
+#define LED_GPIO_PORT GPIOC
+#define LED_GPIO_CLK_ENABLE() __HAL_RCC_GPIOC_CLK_ENABLE()
+
+#else
+#define LED_PIN GPIO_PIN_5
+#define LED_GPIO_PORT GPIOA
+#define LED_GPIO_CLK_ENABLE() __HAL_RCC_GPIOA_CLK_ENABLE()
+#endif
+
+int main(void)
+{
+#ifdef BLUEPILL
+  // SystemCoreClock = 8000000;
+#endif
+  HAL_Init();
+  SystemClock_Config();
+
+  LED_GPIO_CLK_ENABLE();
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  GPIO_InitStruct.Pin = LED_PIN;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(LED_GPIO_PORT, &GPIO_InitStruct);
+  int counter = 10;
+
+  while (1)
+  {
+    HAL_GPIO_TogglePin(LED_GPIO_PORT, LED_PIN);
+
+    HAL_Delay(counter);
+    counter *= 1.2;
+    if (counter > 500)
+      counter = 10;
+  }
+}
+
+void SysTick_Handler(void)
+{
+  HAL_IncTick();
+}
+
+void NMI_Handler(void)
+{
+}
+
+void HardFault_Handler(void)
+{
+  while (1)
+  {
+  }
+}
+
+void MemManage_Handler(void)
+{
+  while (1)
+  {
+  }
+}
+
+void BusFault_Handler(void)
+{
+  while (1)
+  {
+  }
+}
+
+void UsageFault_Handler(void)
+{
+  while (1)
+  {
+  }
+}
+
+void SVC_Handler(void)
+{
+}
+
+void DebugMon_Handler(void)
+{
+}
+
+void PendSV_Handler(void)
+{
+}
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+   * in the RCC_OscInitTypeDef structure.
+   */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    // Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    // Error_Handler();
+  }
+}
